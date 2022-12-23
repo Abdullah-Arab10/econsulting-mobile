1b51fde7e9301418292093766be0d0624b7b8602
Abdullah Arab <abdullahbarab7@gmail.com>
2022-12-23 20:39:06 +0300

    Cashed Images

--- lib/data/models/home_model/home_data_model.dart
+++ lib/data/models/home_model/home_data_model.dart
@@ -1,44 +1,39 @@
-
-
-class HomeDataModel
-{
-  static List<Map<String,dynamic>> doctors = [];
-  static List<Map<String,dynamic>> dentists = [];
-  static List<Map<String,dynamic>> therapists = [];
-  static List<Map<String,dynamic>> lawyers = [];
-  static List<Map<String,dynamic>> economists = [];
-  static List<Map<String,dynamic>> software_engineers = [];
-  static List<Map<String,dynamic>> civil_engineers = [];
+class HomeDataModel {
+  static List<Map<String, dynamic>> doctors = [];
+  static List<Map<String, dynamic>> dentists = [];
+  static List<Map<String, dynamic>> therapists = [];
+  static List<Map<String, dynamic>> lawyers = [];
+  static List<Map<String, dynamic>> economists = [];
+  static List<Map<String, dynamic>> software_engineers = [];
+  static List<Map<String, dynamic>> civil_engineers = [];
 
   HomeDataModel();
 
-  HomeDataModel.fromJson(Map<String, dynamic> json)
-  {
-      json['doctors'].forEach((element){
-        doctors.add(element);
-      });
-
-    json['dentists'].forEach((element){
+  HomeDataModel.fromJson(Map<String, dynamic> json) {
+    json['doctors'].forEach((element) {
+      doctors.add(element);
+    });
+    json['dentists'].forEach((element) {
       dentists.add(element);
     });
 
-    json['therapists'].forEach((element){
+    json['therapists'].forEach((element) {
       therapists.add(element);
     });
 
-    json['lawyers'].forEach((element){
+    json['lawyers'].forEach((element) {
       lawyers.add(element);
     });
 
-    json['economists'].forEach((element){
+    json['economists'].forEach((element) {
       economists.add(element);
     });
 
-    json['software_engineers'].forEach((element){
+    json['software_engineers'].forEach((element) {
       software_engineers.add(element);
     });
 
-    json['civil_engineers'].forEach((element){
+    json['civil_engineers'].forEach((element) {
       civil_engineers.add(element);
     });
   }
--- lib/data/models/home_model/home_model.dart
+++ lib/data/models/home_model/home_model.dart
@@ -1,13 +1,11 @@
 import 'package:e_consulting_flutter/data/models/home_model/home_data_model.dart';
 
-class HomeModel
-{
+class HomeModel {
   static HomeDataModel? data;
 
   HomeModel();
 
-  HomeModel.fromJson(Map<String, dynamic> json)
-  {
-    data = HomeDataModel.fromJson(json['data']);
+  HomeModel.fromJson(Map<String, dynamic> json) {
+    data = HomeDataModel.fromJson(json["data"]);
   }
 }
